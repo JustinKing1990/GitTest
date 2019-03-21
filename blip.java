9ab67d23020aeb5d23320cf72506cb434abfa6eb
King <kingjust@iu.edu>
2019-03-21 14:01:33 -0400

    Edit source

--- blip.java
+++ blip.java
@@ -2,18 +2,18 @@ import java.util.*;
 public class blip{
 public static void main(String[] args){
 	Scanner inputNumber = new Scanner(System.in);
-	int testcase = Integer.parseInt(args[0]);
+	int testcase = Integer.parseInt(args[0]);//adds the input numbers to be checked
 	
-	for (int i = 0; i < testcase ; i++){
+	for (int i = 0; i < testcase ; i++){ 
 		int numberToCheck = inputNumber.nextInt();
 
-		if(numberToCheck < -10 || numberToCheck > 10){
+		if(numberToCheck < -10 || numberToCheck > 10){//determines is the variable is within the parameters
 			System.out.println("The input number is outside of parameters");
 			break;
 			}//if		
  		boolean isEven = false;
 		
-			if(numberToCheck == (int) (2* (numberToCheck/2))){
+			if(numberToCheck == (int) (2* (numberToCheck/2))){//tests to see if the number is even or odd
 			isEven = true;
 			}//if
 		System.out.println(isEven);
